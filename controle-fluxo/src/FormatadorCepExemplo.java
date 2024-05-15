12ac7fa9d8a2c925fb4c1f0c624f76075ffe86dc
Sarah <sarahalvesborges@outlook.com>
2024-05-15 15:44:46 -0300

    feat: add exemplo estruturas excepcionais

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,19 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) throws Exception {
+       try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+       } catch (CepInvalidoException e) {
+            System.out.println("O CEP não corresponde com as regras de negócio.");
+       }
+    }
+
+    public static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8)
+        throw new CepInvalidoException();
+
+        // simulando um cep formatado
+        return "23.765-064";
+
+    }
+}
